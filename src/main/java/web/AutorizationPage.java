51168a51445897d01cf9c3365a2257b78f03f3d5
Artem Krutikov <mastersclean54@gmail.com>
2025-06-21 00:06:01 +0700

    Initial commit

---
+++ src/main/java/web/AutorizationPage.java
@@ -0,0 +1,26 @@
+package web;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import java.net.URL;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class AutorizationPage {
+
+    private SelenideElement inputEmail = $x("//input[@id='e=m=a=i=l']");
+    private SelenideElement inputPassword = $x("//input[@id='password']");
+    private SelenideElement buttonSubmit = $x("//button[@type='submit']");
+
+    public AutorizationPage(String url) {
+        Selenide.open(url);
+    }
+
+    public MainPage autorization(String login, String password){
+        inputEmail.val(login);
+        inputPassword.val(password);
+        buttonSubmit.click();
+        return new MainPage();
+    }
+}
---
+++ src/main/java/web/MainPage.java
@@ -0,0 +1,23 @@
+package web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage {
+
+    private SelenideElement hrefSearch = $x("//input[@placeholder='Поиск...']");
+    private SelenideElement hrefMeting = $x("//a[@href='/meetings']");
+    private SelenideElement hrefContacts = $x("//a[@href='/contacts']");
+    private SelenideElement hrefInterests = $x("//a[@href='/interests']");
+
+
+
+    public ContactsPage openContactsPage(){
+        hrefContacts.shouldBe(Condition.visible).click();
+        return new ContactsPage();
+    }
+
+
+}
